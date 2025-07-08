f9baabe6328bda514a7f374c261fae18ceaf0a62
jack1123-1 <97034652+jack1123-1@users.noreply.github.com>
2025-07-08 14:14:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, jsonify
+import random
+import json
+from urllib.parse import quote_plus
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from flask_cors import CORS
+
+username = quote_plus("site")
+password = quote_plus("@sitesignup515@")
+
+app = Flask(__name__)
+CORS(app)
+uri = f"mongodb+srv://{username}:{password}@weather.crydxka.mongodb.net/?retryWrites=true&w=majority&appName=weather"
+client = MongoClient(uri, server_api=ServerApi('1'))    
+
+db = client["weather_database"]
+summary_collection = db["winter_summary"]
+hourly_collection = db["winter_hour"]
+readings_collection = db["winter_readings"]
+
+#try:
+#    client.admin.command('ping')
+#    print("Pinged your deployment. You successfully connected to MongoDB!")
+#except Exception as e:
+#    print(e)
+
+def return_last_update(iterable):
+    last = len(iterable) - 1
+    index = random.randint(0, last)
+    if not iterable:
+        return None
+    return iterable[index]
+#    if not iterable:
+#        return None
+#    return (iterable[0])
+
+def return_previous_update(iterable):
+    last = len(iterable) - 1
+    index = random.randint(1, last)
+    if len(iterable) < 2:
+        return None
+    return iterable[index]
+#    if len(iterable) < 2:
+#        return None
+#    return (iterable[1])
+
+@app.route("/add-weather", methods=["GET"])
+def add_data():
+    with open ("weather_data.json") as f:
+        data = json.load(f)    
+    result = summary_collection.insert_many(data)
+    return f"success", 201
+
+@app.route("/add-readings", methods=["GET"])
+def add_readings():
+    with open("instrument_readings.json") as f:
+        data = json.load(f)
+    result = readings_collection.insert_many(data)
+    return f"success", 201
+
+@app.route("/get/summary", methods=["GET"])
+def get_weather():
+    try:
+        weather_data = list(summary_collection.find({}, {"_id"}))
+    except Exception as e:
+        return f"Error, {e}"
+    return jsonify(weather_data), 200
+
+@app.route("/get-hour", methods=["GET"])
+def get_hour():
+    try:
+        hour_data = list(hourly_collection.find({}, {"_id"}))
+    except Exception as e:
+        return f"Error, {e}"
+    return jsonify(hour_data), 200
+
+@app.route("/get-readings", methods=["GET"])
+def get_readings():
+    try:
+        readings_data = list(readings_collection.find({}, {"_id"}))
+    except Exception as e:
+        return f"Erro, {e}"
+    return jsonify(readings_data), 200
+
+@app.route("/get-changes", methods=["GET"])
+def get_changes():
+    for _ in range(20):
+        try:
+            wind_current_values = list(readings_collection.find({"wind_current": {"$exists": True}}))
+            wind_voltage_values = list(readings_collection.find({"wind_voltage": {"$exists": True}}))
+            solar_current_values = list(readings_collection.find({"solar_current": {"$exists": True}}))
+            solar_voltage_values = list(readings_collection.find({"solar_voltage": {"$exists": True}}))
+            current_wind_current_value = return_last_update(wind_current_values)
+            previous_wind_current_value = return_previous_update(wind_current_values)
+            current_wind_voltage_value = return_last_update(wind_voltage_values)
+            previous_wind_voltage_value = return_previous_update(wind_voltage_values)       
+            current_solar_voltage_value = return_last_update(solar_voltage_values)
+            previous_solar_voltage_value = return_previous_update(solar_voltage_values)
+            current_solar_current_value = return_last_update(solar_current_values)
+            previous_solar_current_value = return_previous_update(solar_current_values)
+            wind_current_change = current_wind_current_value["wind_current"] - previous_wind_current_value["wind_current"]
+            wind_voltage_change = current_wind_voltage_value["wind_voltage"] - previous_wind_voltage_value["wind_voltage"]
+            solar_current_change = current_solar_current_value["solar_current"] - previous_solar_current_value["solar_current"]
+            solar_voltage_change = current_solar_voltage_value["solar_current"] - previous_solar_voltage_value["solar_current"]
+            data = {'wind_current_change': wind_current_change, 'wind_voltage_change': wind_voltage_change,
+                    'solar_current_change': solar_current_change, 'solar_voltage_change': solar_voltage_change
+                    }
+            return data
+        except Exception as e:
+            return f"Error, {e}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
